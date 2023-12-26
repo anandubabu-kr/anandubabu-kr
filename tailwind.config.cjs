61c899785facb41f1e310ec6ee4c0b049a865f34
Anandu Babu <anandubabu.sd@gmail.com>
2023-12-26 22:33:05 +0530

    updated print resume

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    backgroundSize: {
+      "w-full": "100% 3px",
+      "w-none": "0% 3px",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
